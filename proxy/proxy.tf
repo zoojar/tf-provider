b4f4e846e94c7a77ea54873ead6838ef3c675733
zoojar <davidnewton82@gmail.com>
2017-06-16 12:33:20 +0100

    haproxy

---
+++ proxy/proxy.tf
@@ -0,0 +1,75 @@
+
+variable "datacenter"         {}         
+variable "vsphere_user"       {}
+variable "vsphere_password"   {}
+variable "vsphere_server"     {}
+variable "template"           { type = "string" default = "rhel-server-7.3-x86_64_vmtools" }
+variable "memory_mb"          { type = "string" default = "2000" }
+variable "vcpu_count"         { type = "string" default = "2" }
+variable "vm_network"         { type = "string" default = "VM Network" }
+variable "ipv4_address"       {}
+variable "ipv4_prefix_length" { type = "string" default = "24" }
+variable "ipv4_gateway"       {}
+variable "dns_servers"        { type = "list" }
+variable "ssh_username"       {}
+variable "ssh_password"       {}
+variable "hostname"           {}
+
+variable "yumrepo_baseurl"        {}
+variable "puppet_modules_baseurl" {}
+
+# Configure the VMware vSphere Provider
+provider "vsphere" {
+  user                 = "${var.vsphere_user}"
+  password             = "${var.vsphere_password}"
+  vsphere_server       = "${var.vsphere_server}"
+  allow_unverified_ssl = true
+}
+
+resource "vsphere_virtual_machine" "proxy" {
+  name         = "${var.hostname}"
+  vcpu         = "${var.vcpu_count}"
+  memory       = "${var.memory_mb}"
+  datacenter   = "${var.datacenter}"
+  dns_servers  = "${var.dns_servers}"
+
+  network_interface {
+    label              = "${var.vm_network}"
+    ipv4_address       = "${var.ipv4_address}"
+    ipv4_prefix_length = "${var.ipv4_prefix_length}"
+    ipv4_gateway       = "${var.ipv4_gateway}"
+  }
+
+  disk {
+    type     = "thin" 
+    template = "${var.template}" 
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "root"
+    password = "root"
+  }
+
+  ### Environment specific stuff...
+  provisioner "remote-exec" { ### block internet & add route back to vpn
+    inline = [
+      "route add default gw 192.168.0.2",
+      "ip route add 10.8.0.0/24 via 192.168.0.1",
+    ]
+  }
+  ###
+
+  provisioner "file" {
+    source      = "scripts"
+    destination = "/tmp"
+  }
+  
+  provisioner "remote-exec" {
+    inline = [
+      ". /tmp/scripts/configure_yumrepo.sh ${var.yumrepo_baseurl}",
+      ". /tmp/scripts/deploy_proxy.sh --puppet_modules_baseurl=${var.puppet_modules_baseurl} --puppetserver_fqdn=${var.puppetserver_fqdn} --puppetserver_ip=${var.puppetserver_ip} --puppetserver_port=${var.puppetserver_port}",
+    ]
+  }
+
+}
---
+++ scripts/deploy_proxy.sh
@@ -0,0 +1,130 @@
+#!/bin/bash
+# Deploy script for Proxy server, installs, configures & starts HAProxy
+
+#Defaults:
+log_file='/tmp/deploy_proxy.log'
+puppet_package='puppet-agent'
+deploy_proxy_pp='/tmp/deploy_proxy.pp'
+tmp_puppet_modules='/tmp/puppet_modules'
+read -d '' puppet_modules <<'EOF'
+puppetlabs-stdlib-4.17.0.tar.gz
+puppetlabs-haproxy-1.5.0.tar.gz
+puppetlabs-firewall-1.8.2.tar.gz
+EOF
+
+while test $# -gt 0; do
+        case "$1" in
+                -h|--help)
+                        echo "options:"
+                        echo "-h, --help                        Show help."
+                        echo "-m, --puppet_modules_baseurl      Base URL for downloading Puppet modules, example: http://repohost.local/puppet_modules"
+                        echo "-f, --puppetserver_fqdn           Puppetserver FQDN (balancermember)"
+                        echo "-i, --puppetserver_ip             Puppetserver ip (balancermember)"
+                        echo "-p, --puppetserver_port           Puppetserver port (balancermember)"
+                        exit 0
+                        ;;
+                -f)
+                        shift
+                        if test $# -gt 0; then
+                                puppetserver_fqdn=$1
+                        else
+                                echo "ERROR: --puppetserver_fqdn|-f NOT specified."
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --puppetserver_fqdn*)
+                        puppetserver_fqdn=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;  
+                -i)
+                        shift
+                        if test $# -gt 0; then
+                                puppetserver_ip=$1
+                        else
+                                echo "ERROR: --puppetserver_ip|-i NOT specified."
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --puppetserver_ip*)
+                        puppetserver_ip=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;  
+                -m)
+                        shift
+                        if test $# -gt 0; then
+                                puppet_modules_baseurl=$1
+                        else
+                                echo "ERROR: --puppet_modules_baseurl|-m NOT specified."
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --puppet_modules_baseurl*)
+                        puppet_modules_baseurl=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;  
+                -p)
+                        shift
+                        if test $# -gt 0; then
+                                puppetserver_port=$1
+                        else
+                                echo "ERROR: --puppetserver_port|-p NOT specified."
+                                exit 1
+                        fi
+                        shift
+                        ;;
+                --puppetserver_port*)
+                        puppetserver_port=`echo $1 | sed -e 's/^[^=]*=//g'`
+                        shift
+                        ;;  
+                *)
+                        break
+                        ;;
+        esac
+done
+
+firewall_default_zone=`firewall-cmd --get-default-zone`
+echo "$(date) INFO: Configuring firewall: Opening ports 80, 443 & 22 for the default zone: ${firewall_default_zone}..." | tee -a  $log_file
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=80/tcp
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=443/tcp
+firewall-cmd --permanent --zone=$firewall_default_zone --add-port=22/tcp
+firewall-cmd --reload
+
+echo "$(date) INFO: Installing [$puppet-agent] via YUM..." | tee -a  $log_file
+yum install -y $puppet_package
+export PATH="$PATH:/opt/puppetlabs/bin"
+
+echo "$(date) INFO: Downloading & installing Puppet modules from [$puppet_modules_baseurl]" | tee -a  $log_file
+puppet resource file $tmp_puppet_modules ensure=directory
+IFS=$'\n'
+for module in $puppet_modules ; do
+  puppet resource file $tmp_puppet_modules/$module ensure=file source=$puppet_modules_baseurl/$module
+  puppet module install $tmp_puppet_modules/$module --ignore-dependencies
+done
+
+echo "$(date) INFO: Deploying HAProxy via Puppet..." | tee -a  $log_file
+cat >$deploy_proxy_pp <<EOF
+  include ::haproxy
+  haproxy::listen { 'puppetserver':
+    collect_exported => false,
+    ipaddress        => \$::ipaddress,
+    ports            => '$puppetserver_port',
+  }
+  haproxy::balancermember { 'puppetserver01':
+    listening_service => 'puppetserver',
+    server_names      => '$puppetserver_fqdn',
+    ipaddresses       => '$puppetserver_ip',
+    ports             => '$puppetserver_port',
+    options           => 'check',
+  }
+  firewall { '814 acc tcp dport $puppetserver_port': proto  => 'tcp', dport  => $puppetserver_port, action => 'accept' } 
+  class {'firewall':
+    ensure => stopped,
+  }
+EOF
+export PATH="/opt/puppetlabs/bin:/opt/puppetlabs/puppet/bin:/opt/puppet/bin:$PATH"
+puppet apply $deploy_proxy_pp -v
+
+
