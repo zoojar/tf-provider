1153a5f1bfa49d8c4ab376ba231f8febdccca28d
zoojar <davidnewton82@gmail.com>
2017-06-30 09:34:15 +0100

    staging control-repo dir

--- puppetserver/puppetserver.tf
+++ puppetserver/puppetserver.tf
@@ -85,7 +85,7 @@ resource "vsphere_virtual_machine" "puppetserver" {
 
   provisioner "file" {
     source      = "../control-repo-staging/production/"
-    destination = "${var.puppet_codedir}/environments/production/"
+    destination = "${var.puppet_codedir}/staging_control-repo/"
   }
 
   provisioner "file" {
@@ -96,7 +96,7 @@ resource "vsphere_virtual_machine" "puppetserver" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x /etc/puppetlabs/code/environments/production/scripts/*",
-      "FACTER_masterless=true /opt/puppetlabs/bin/puppet apply -e \"include roles::puppetserver\" --modulepath=/etc/puppetlabs/code/modules:/etc/puppetlabs/code/environments/production/site:/etc/puppetlabs/code/environments/production/modules",
+      "FACTER_masterless=true /opt/puppetlabs/bin/puppet apply -e \"include roles::puppetserver\" --modulepath=${var.puppet_codedir}/modules:${var.puppet_codedir}/staging_control-repo",
     ]
   }
 }
