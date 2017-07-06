16b0c52a5f0215c17fd8e73c080bf8d3e9c9a37c
zoojar <davidnewton82@gmail.com>
2017-07-06 14:35:26 +0100

    repohost fqdn & ip

--- agent/agent.tf
+++ agent/agent.tf
@@ -18,6 +18,9 @@ variable "hostname"           {}
 variable "yumrepo_baseurl"        {}
 variable "puppetserver_fqdn"      {}
 variable "puppetserver_ip"        {}
+variable "repohost_fqdn"          {}
+variable "repohost_ip"            {}
+variable "puppetserver_ip"        {}
 variable "psk"                    {}
 variable "role"                   {}
 variable "init_script"            { type = "string" default = "ls" }
@@ -74,6 +77,7 @@ resource "vsphere_virtual_machine" "agent" {
     inline = [
       "${var.init_script}",
       ". /tmp/scripts/configure_yumrepo.sh ${var.yumrepo_baseurl}",
+      ". /tmp/scripts/set_etc_hosts.sh ${var.puppetserver_ip} ${var.puppetserver_fqdn}",
       ". /tmp/scripts/set_etc_hosts.sh ${var.puppetserver_ip} ${var.puppetserver_fqdn}",
       ". /tmp/scripts/install_puppetagent.sh --puppetserver_fqdn=${var.puppetserver_fqdn} --psk=${var.psk} --role=${var.role}",
     ]
