eff7dd5b0c8ede30376db32a64c1d34649013806
zoojar <davidnewton82@gmail.com>
2017-10-02 11:28:04 +0100

    domain added for windows boxes - certname

--- scripts/install_puppet_agent.ps1
+++ scripts/install_puppet_agent.ps1
@@ -3,13 +3,14 @@ param (
   [string]$puppet_master_server = $(throw "-puppet_master_server is required."),
   [string]$installer_url = $(throw "-installer_url is required."),
   [string]$role = $(throw "-role is required."),
-  [string]$psk = $(throw "-psk is required.")
+  [string]$psk = $(throw "-psk is required."),
+  [string]$puppet_agent_certname = $(throw "-puppet_agent_certname is required.")
 )
 
 mkdir c:\programdata\puppetlabs\puppet\etc
 set-content c:\Programdata\PuppetLabs\Puppet\etc\csr_attributes.yaml "custom_attributes:`r`n 1.2.840.113549.1.9.7: $psk`r`nextension_requests:`r`n  pp_role: $role`r`n"
 wget $installer_url -outfile c:\\windows\\temp\\puppet-enterprise-installer.msi
 $msiexec_path = "C:\Windows\System32\msiexec.exe"
-$msiexec_args = "/qn /log c:\\windows\\temp\\puppet-enterprise-installer.log /i c:\\windows\\temp\\puppet-enterprise-installer.msi PUPPET_MASTER_SERVER=${puppet_master_server}"
+$msiexec_args = "/qn /log c:\\windows\\temp\\puppet-enterprise-installer.log /i c:\\windows\\temp\\puppet-enterprise-installer.msi PUPPET_AGENT_CERTNAME=${puppet_agent_certname} PUPPET_MASTER_SERVER=${puppet_master_server}"
 $msiexec_proc = [System.Diagnostics.Process]::Start($msiexec_path, $msiexec_args)
 $msiexec_proc.WaitForExit()
