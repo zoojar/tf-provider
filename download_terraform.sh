37c38906dc50bd9e8f5a069c6dfd35b28d490c02
David Newton <davidnewton82@gmail.com>
2018-05-22 09:33:01 +0100

    Update download_terraform.sh

--- download_terraform.sh
+++ download_terraform.sh
@@ -12,6 +12,7 @@ fi
 #download & check sha256sum:
 wget -k https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_SHA256SUMS
 if ! cat terraform_${TF_VERSION}_SHA256SUMS | grep $TF_FILE | sha256sum -c ; then 
+  rm -f ./${TF_FILE}
   wget -k https://releases.hashicorp.com/terraform/${TF_VERSION}/${TF_FILE}
   #replace cached file:
   yes | cp $TF_FILE ${TF_BIN_CACHE_DIR}/${TF_FILE}
