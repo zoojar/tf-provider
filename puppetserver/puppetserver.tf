f702ca20d20bf5c6f66c61bdcdfd1fb40fde5814
zoojar <david@tokyocoffee.home>
2018-05-22 07:10:05 +0100

    r10k key prep

--- puppetserver/puppetserver.tf
+++ puppetserver/puppetserver.tf
@@ -99,7 +99,7 @@ resource "vsphere_virtual_machine" "puppetserver" {
   provisioner "remote-exec" {
     inline = [
       "chmod +x ${var.staging_code_dir}/scripts/*",
-      "puppet resource file /etc/puppetlabs/r10k ensure=directory && puppet resource file /etc/puppetlabs/r10k/r10k_id_rsa mode='0400' content='${var.r10k_sshkey_file_content}'",
+      #"puppet resource file /etc/puppetlabs/r10k ensure=directory && puppet resource file /etc/puppetlabs/r10k/r10k_id_rsa mode='0400' content='${var.r10k_sshkey_file_content}'",
       "FACTER_staging_puppetserver=true /opt/puppetlabs/bin/puppet apply -e \"include roles::puppetserver\" --hiera_config=${var.staging_code_dir}/hiera.yaml --modulepath=${var.staging_code_dir}/site:${var.staging_code_dir}/site/profiles/files/puppetserver/r10k_module_dependencies:${var.staging_code_dir}/modules",
       "rm -rf /etc/puppetlabs/puppet/ssl",
       "rm -f /etc/puppetlabs/puppetserver/ssl/ca/signed/*.pem",
